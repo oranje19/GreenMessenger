f538322e4bd1b314de420b0ddc786d9b0317b885
oranje19 <jasonwintery@gmail.com>
2021-02-18 19:59:36 -0500

    Chat is saved in DB

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,21 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import './App.css';
 import Chat from "./components/chat/chat";
 import Sidebar from "./components/sidebar/sidebar";
 import Pusher from 'pusher-js';
+import axios from "./axios";
 
 function App() {
+
+  const [messages, setMessages] = useState([]);
+
+  // to fetch
   useEffect(() => {
-    
-  })
+    axios.get('/messages/sync')
+      .then(response => {
+        setMessages(response.data)
+      })
+  }, []);
 
   useEffect(() => {
     const pusher = new Pusher('d34e50dcc3d128876a62', {
@@ -15,16 +23,25 @@ function App() {
     });
 
     const channel = pusher.subscribe('messages');
-    channel.bind('inserted', (data) => {
-      alert(JSON.stringify(data));
+    channel.bind('inserted', (newMessage) => {
+      // alert(JSON.stringify(newMessage));
+      setMessages([...messages, newMessage])
     });
-  }, [])
+
+    return () => {
+      channel.unbind_all();
+      channel.unsubscribe();
+    }
+
+  }, [messages])
+
+  console.log(messages)
 
   return (
     <div className="app">
         <div className="app__body">
           <Sidebar />
-          <Chat />
+          <Chat messages={messages} />
         </div>
     </div>
   );
--- frontend/src/components/chat/chat.js
+++ frontend/src/components/chat/chat.js
@@ -1,13 +1,36 @@
 import { Avatar, IconButton } from '@material-ui/core';
 import { AttachFile, InsertEmoticon, Mic, MoreVert, SearchOutlined } from '@material-ui/icons';
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+import axios from "../../axios";
 import "./chat.css";
 
-function Chat() {
+function Chat({ messages }) {
+
+    const [input, setInput] = useState('');
+    const [seed, setSeed] = useState('');
+
+    // prevent the default / refresh
+    const sendMessage = async (e) => {
+        e.preventDefault();
+
+        await axios.post('/messages/new', {
+            message: input,
+            name: "Demo App",
+            timestamp: "Just now!",
+            received: false
+        });
+
+        setInput('');
+    };
+
+    useEffect(() => {
+        setSeed(Math.floor(Math.random() * 5000));
+    }, []);
+
     return (
         <div className="chat">
             <div className="chat__header">
-                <Avatar />
+                <Avatar src={`https://avatars.dicebear.com/api/human/${seed}.svg`} />
 
                 <div className="chat__headerInfo">
                     <h3>Room name</h3>
@@ -28,21 +51,23 @@ function Chat() {
             </div>
 
             <div className="chat__body">
-                <p className="chat__message">
-                    <span className="chat__name">Jason</span>
-                    This is a message
-                    <span className="chat__timestamp">
-                        {new Date().toUTCString()}
-                    </span>
-                </p>
+                {messages.map(message => (
+                    <p className={`chat__message ${message.received && "chat__receiver"}`}>
+                        <span className="chat__name">{message.name}</span>
+                        {message.message}
+                        <span className="chat__timestamp">
+                            {message.timestamp}
+                        </span>
+                    </p>
+                ))}
 
-                <p className="chat__message chat__receiver">
+                {/* <p className="chat__message chat__receiver">
                     <span className="chat__name">Jason</span>
                     This is a message
                     <span className="chat__timestamp">
                         {new Date().toUTCString()}
                     </span>
-                </p>
+                </p> */}
 
             </div>
 
@@ -50,10 +75,13 @@ function Chat() {
                 <InsertEmoticon />
                 <form>
                     <input
+                        value={input}
+                        onChange={e => setInput(e.target.value)}
                         placeholder="Type a message"
                         type="text"
                     />
                     <button
+                        onClick={sendMessage}
                         type="submit"
                     >
                         Send a message
@@ -66,3 +94,6 @@ function Chat() {
 }
 
 export default Chat;
+
+
+// e.target.value means the latest entry to be type in
