e91bd760fdf81b4a7aa6ba1028c14ee4040f3e43
oranje19 <jasonwintery@gmail.com>
2021-02-19 03:50:55 -0500

    Add Icon and Footers

--- frontend/src/components/login/login.js
+++ frontend/src/components/login/login.js
@@ -1,9 +1,10 @@
 import React from 'react';
-import { Button } from '@material-ui/core';
+import { Button, IconButton } from '@material-ui/core';
 import "./login.css";
 import { auth, provider } from '../../firebase';
 import { useStateValue } from '../../stateProvider';
 import { actionTypes } from '../../reducer';
+import { GitHub, Language, LinkedIn } from '@material-ui/icons';
 
 function Login() {
     const [{}, dispatch] = useStateValue();
@@ -33,6 +34,23 @@ function Login() {
                 <Button onClick={signIn}>
                     Sign In With Google
                 </Button>
+                <div className="login__footer">
+                    <IconButton>
+                        <a target="_blank" href="https://www.linkedin.com/in/jasonwintery/">
+                            <LinkedIn className="login__footer__linkedin" src="https://www.linkedin.com/in/jasonwintery/" />
+                        </a>
+                    </IconButton>
+                    <IconButton>
+                        <a target="_blank" href="https://github.com/oranje19">
+                            <GitHub className="login__footer__github" src="https://www.linkedin.com/in/jasonwintery/" />
+                        </a>
+                    </IconButton>
+                    <IconButton>
+                        <a target="_blank" href="http://www.jasonwintery.com/">
+                            <Language className="login__footer__portfolio" src="https://www.linkedin.com/in/jasonwintery/" />
+                        </a>
+                    </IconButton>
+                </div>
             </div>
         </div>
     );
